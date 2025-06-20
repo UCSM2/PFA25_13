68fe9bbf82b3c60f0ef7c0eaef75eda04ec9a470
DiegoFlores24 <diego.floresg@estudiante.ucsm.edu.pe>
2025-06-19 20:49:19 -0500

    Update CiudadGrafo.java

--- modelo/CiudadGrafo.java
+++ modelo/CiudadGrafo.java
@@ -1,5 +1,33 @@
 package modelo;
 
+import java.util.*;
+
 public class CiudadGrafo {
+        private Map<String, Zona> zonas;
+
+    public CiudadGrafo() {
+        zonas = new HashMap<>();
+    }
+
+    public void agregarZona(String nombre) {
+        zonas.putIfAbsent(nombre, new Zona(nombre));
+    }
+
+    public void agregarVia(String origen, String destino, int peso) {
+        Zona z1 = zonas.get(origen);
+        Zona z2 = zonas.get(destino);
+        if (z1 != null && z2 != null) {
+            z1.conectarCon(z2, peso);
+        }
+    }
+
+    public void eliminarVia(String origen, String destino) {
+        Zona z1 = zonas.get(origen);
+        Zona z2 = zonas.get(destino);
+        if (z1 != null && z2 != null) {
+            z1.getConexiones().remove(z2);
+        }
+    }
+
     
 }
